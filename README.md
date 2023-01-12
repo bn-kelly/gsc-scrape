c51c6e9937c0f3e4b4f00291dc763ce1295bb67c
bnkelly <briankellyupwk@gmail.com>
2023-01-12 22:03:28 +0200

    scrape links from google search console

--- README.md
+++ README.md
@@ -1,27 +1,10 @@
-# gsc-api
+# gsc-scrape
 
 ## Overview
 This script is to download external link Urls from Google search console.
 
 ## Requirement
 Python 3.7+
-
-## Get a key file
-1. Open Google Cloud Platform and choose your project.
-2. Click `APIs & Services` -> `Credentials` in the left menu.
-![](./images/1.PNG)
-3. Click `+CREATE CREDENTIALS` and choose `Service account`.
-![](./images/2.PNG)
-4. Put Service account name and click `Done`.
-![](./images/3.PNG)
-5. Clicking the created service account will take you to the service account detail page.
-![](./images/4.PNG)
-6. Click `KEYS` tab and create a key file as a JSON format by clicking `ADD KEY`, and download it.
-![](./images/5.PNG)
-7. Open Google Search Console and choose the website in the top-left.
-8. Move to settings page by clicking `Settings` in the left menu.
-9. Click `Users and permissions` and add the service account there with `Full` permission.
-
 
 ## Install dependencies
 ```commandline
@@ -33,7 +16,5 @@ pip install -r requirements.txt
 python script.py
 
 Site url: https://www.example.com/  (Make sure you have the website in Google Search Console)
-Start date: xxxx-xx-xx
-End date: xxxx-xx-xx
-Key file path: client_secrets.json  (You need to get the key file from Google Cloud Console)
+Profile directory: /home/brian/.mozilla/firefox/q6h997k2.default-release-1646795451268 (Firefox profile directory)
 ```
--- script.py
+++ script.py
@@ -1,26 +1,79 @@
-from googleapiclient.discovery import build
-from google.oauth2.service_account import Credentials
+import re
+import time
+import urllib.parse
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from webdriver_manager.firefox import GeckoDriverManager
+from pyvirtualdisplay import Display
+from constants import x_target_urls, x_sites, x_page, x_target_url_if_different, x_btn_next, x_domains
+
+
+def get_num_pages(total, limit_per_page):
+    return int(total / limit_per_page) if total % limit_per_page == 0 else int(total / limit_per_page + 1)
 
 
 def main():
-    site_url = input('Site url:')
-    start_date = input('Start date:')
-    end_date = input('End date:')
-    key_file = input('Key file path:')
-    scopes = ['https://www.googleapis.com/auth/webmasters.readonly']
+    site_url = 'https://brickfilms.com/'
+    url = f'https://search.google.com/search-console/links/drilldown?resource_id={urllib.parse.quote(site_url)}&type=EXTERNAL&target=&domain='
+    profile_dir = '/home/osboxes/.mozilla/firefox/q6h997k2.default-release-1646795451268'
 
-    creds = Credentials.from_service_account_file(key_file, scopes=scopes)
-    service = build('webmasters', 'v3', credentials=creds)
+    # setup display
+    display = Display(visible=True, size=(1920, 1080))
+    display.start()
+    # init firefox profile
+    profile = webdriver.FirefoxProfile(profile_dir)
+    profile.set_preference("dom.webdriver.enabled", False)
+    profile.set_preference('useAutomationExtension', False)
+    profile.update_preferences()
+    # init driver
+    options = webdriver.FirefoxOptions()
+    options.binary_location = '/usr/bin/firefox'
+    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(),
+                               firefox_profile=profile,
+                               desired_capabilities=DesiredCapabilities.FIREFOX,
+                               options=options)
+    driver.maximize_window()
 
-    body = {
-        'startDate': start_date,
-        'endDate': end_date,
-        'dimensions': ['date', 'page']
-    }
-    response = service.searchanalytics().query(siteUrl=site_url, body=body).execute()
-    if 'rows' in response:
-        for row in response['rows']:
-            print(row['page'])
+    # open initial page
+    driver.get(url)
+
+    target_urls = [el.text for el in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x_target_urls)))]
+    num_target_pages = get_num_pages(len(target_urls), 25)
+    for i in range(num_target_pages):
+        target_urls = [re.sub('\\s+', '', el.text) for el in
+                       WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x_target_urls)))]
+        target_urls = [x for x in target_urls if x != '']
+
+        for target_url in target_urls:
+            target_page = [x for x in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, f'{x_target_urls}[contains(., "{target_url}")]'))) if x.is_displayed()]
+            time.sleep(0.5)
+            target_page[0].click()
+            domains = [el.text for el in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x_domains)))]
+            num_domain_pages = get_num_pages(len(domains), 25)
+
+            for j in range(num_domain_pages):
+                domains = [re.sub('\\s+', '', el.text) for el in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x_domains)))]
+                domains = [x for x in domains if x != '']
+                for domain in domains:
+                    site = [x for x in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, f'{x_sites}[contains(., "{domain}")]'))) if x.is_displayed()]
+                    time.sleep(0.5)
+                    site[0].click()
+
+                    page = re.sub('\\s+', '', WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, x_page))).text)
+                    target_url_if_different = re.sub('\\s+', '', WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, x_target_url_if_different))).text)
+                    print(f'Target Url: {target_url}, Linking page: {page}, Target URL (if different): {target_url_if_different}')
+
+                    driver.back()
+                    time.sleep(0.5)
+                WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, x_btn_next))).click()
+            driver.back()
+            time.sleep(0.5)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, x_btn_next))).click()
+
+    driver.close()
 
 
 if __name__ == '__main__':
