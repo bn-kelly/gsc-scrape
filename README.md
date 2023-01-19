c2d50ab58e76d38741e7329b6b27b08f49aebb9f
bnkelly <briankellyupwk@gmail.com>
2023-01-19 15:52:15 +0200

    update script.py, readme

--- README.md
+++ README.md
@@ -6,17 +6,26 @@ This script is to download external link Urls from Google search console.
 ## Requirement
 Python 3.7+
 
+## Download
+```
+$ git clone https://github.com/bnkelly/gsc-scrape.git
+```
+
 ## Environment Setup
 ```commandline
-python3 -m venv venv
-./venv/bin/activate.bat
-pip install -r requirements.txt
+$ cd gsc-scrape
+$ python3 -m venv venv
+$ source ./venv/bin/activate
+$ pip install -r requirements.txt
 ```
 
 ## Run
 ```commandline
 python script.py
 
-Site url: https://www.example.com/  (Make sure you have the website in Google Search Console)
-Profile directory: /home/brian/.mozilla/firefox/q6h997k2.default-release-1646795451268 (Firefox profile directory)
+site url: https://www.example.com/  (Make sure you have the website in Google Search Console)
+firefox binary path: /snap/firefox/1635/usr/lib/firefox/firefox (Firefox binary path)
+firefox profile path: /home/measurable/snap/firefox/common/.mozilla/firefox/26i5plou.default (Firefox profile path)
+output file: output.csv (Output file name)
 ```
+The message `finished` will be shown when the script finished the scraping
--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import re
 import time
 import urllib.parse
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,13 +17,16 @@ def get_num_pages(total, limit_per_page):
 
 
 def main():
-    site_url = 'https://brickfilms.com/'
+    site_url = input('site url:')
+    binary_location = input('firefox binary path:')
+    profile_dir = input('firefox profile path:')
+    output_file = input('output file:')
     url = f'https://search.google.com/search-console/links/drilldown?resource_id={urllib.parse.quote(site_url)}&type=EXTERNAL&target=&domain='
-    profile_dir = '/home/brian/.mozilla/firefox/q6h997k2.default-release-1646795451268'
+    header = ['Target Url', 'Linking page', 'Target URL (if different)']
 
     # setup display
-    display = Display(visible=True, size=(1920, 1080))
-    display.start()
+    # display = Display(visible=True, size=(1920, 1080))
+    # display.start()
     # init firefox profile
     profile = webdriver.FirefoxProfile(profile_dir)
     profile.set_preference("dom.webdriver.enabled", False)
@@ -30,7 +34,7 @@ def main():
     profile.update_preferences()
     # init driver
     options = webdriver.FirefoxOptions()
-    options.binary_location = GeckoDriverManager().install()
+    options.binary_location = binary_location
     options.headless = True
     driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(),
                                firefox_profile=profile,
@@ -41,39 +45,44 @@ def main():
     # open initial page
     driver.get(url)
 
-    target_urls = [el.text for el in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x_target_urls)))]
-    num_target_pages = get_num_pages(len(target_urls), 25)
-    for i in range(num_target_pages):
-        target_urls = [re.sub('\\s+', '', el.text) for el in
-                       WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x_target_urls)))]
-        target_urls = [x for x in target_urls if x != '']
+    with open(output_file, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        
+        target_urls = [el.text for el in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x_target_urls)))]
+        num_target_pages = get_num_pages(len(target_urls), 25)
+        for i in range(num_target_pages):
+            target_urls = [re.sub('\\s+', '', el.text) for el in
+                        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x_target_urls)))]
+            target_urls = [x for x in target_urls if x != '']
 
-        for target_url in target_urls:
-            target_page = [x for x in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, f'{x_target_urls}[contains(., "{target_url}")]'))) if x.is_displayed()]
-            time.sleep(0.5)
-            target_page[0].click()
-            domains = [el.text for el in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x_domains)))]
-            num_domain_pages = get_num_pages(len(domains), 25)
+            for target_url in target_urls:
+                target_page = [x for x in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, f'{x_target_urls}[contains(., "{target_url}")]'))) if x.is_displayed()]
+                time.sleep(0.5)
+                target_page[0].click()
+                domains = [el.text for el in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x_domains)))]
+                num_domain_pages = get_num_pages(len(domains), 25)
 
-            for j in range(num_domain_pages):
-                domains = [re.sub('\\s+', '', el.text) for el in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x_domains)))]
-                domains = [x for x in domains if x != '']
-                for domain in domains:
-                    site = [x for x in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, f'{x_sites}[contains(., "{domain}")]'))) if x.is_displayed()]
-                    time.sleep(0.5)
-                    site[0].click()
+                for j in range(num_domain_pages):
+                    domains = [re.sub('\\s+', '', el.text) for el in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x_domains)))]
+                    domains = [x for x in domains if x != '']
+                    for domain in domains:
+                        site = [x for x in WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, f'{x_sites}[contains(., "{domain}")]'))) if x.is_displayed()]
+                        time.sleep(0.5)
+                        site[0].click()
 
-                    page = re.sub('\\s+', '', WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, x_page))).text)
-                    target_url_if_different = re.sub('\\s+', '', WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, x_target_url_if_different))).text)
-                    print(f'Target Url: {target_url}, Linking page: {page}, Target URL (if different): {target_url_if_different}')
+                        page = re.sub('\\s+', '', WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, x_page))).text)
+                        target_url_if_different = re.sub('\\s+', '', WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, x_target_url_if_different))).text)
+                        writer.writerow([target_url, page, target_url_if_different])
 
-                    driver.back()
-                    time.sleep(0.5)
-                WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, x_btn_next))).click()
-            driver.back()
-            time.sleep(0.5)
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, x_btn_next))).click()
+                        driver.back()
+                        time.sleep(0.5)
+                    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, x_btn_next))).click()
+                driver.back()
+                time.sleep(0.5)
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, x_btn_next))).click()
 
+    print('finished')
     driver.close()
 
 
